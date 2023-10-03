d80751a3368e3521e589c8e437576757378b31f7
Boyi Shi <adrian.boyishi@gmail.com>
2023-10-03 00:38:03 -0500

    Added Vault

--- src/main/java/com/boyishi/demo/DemoApplication.java
+++ src/main/java/com/boyishi/demo/DemoApplication.java
@@ -1,12 +1,23 @@
 package com.boyishi.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableConfigurationProperties(PostgresConfig.class)
+public class DemoApplication implements CommandLineRunner {
 	private static ApplicationContext applicationContext;
+
+	private final PostgresConfig configuration;
+
+	public DemoApplication(PostgresConfig configuration) {
+		this.configuration = configuration;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -18,6 +29,18 @@ public class DemoApplication {
 			System.out
 					.println("Is " + beanName + " in ApplicationContext: " + applicationContext.containsBean(beanName));
 		}
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+		logger.info("----------------------------------------");
+		logger.info("Configuration properties");
+		logger.info("   example.username is {}", configuration.getUsername());
+		logger.info("   example.password is {}", configuration.getPassword());
+		logger.info("----------------------------------------");
 	}
 
 }
